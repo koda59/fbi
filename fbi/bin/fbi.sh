409a8597de478e46daaebc7e85545211979f3d05
koda59 <33828204+koda59@users.noreply.github.com>
2018-12-01 13:34:13 +0100

    Update fbi.sh

--- fbi/bin/fbi.sh
+++ fbi/bin/fbi.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# 2018-12-01 Koda59	v1.1
+# 2018-12-01 Koda59	v1.0
 #
 
 ###
